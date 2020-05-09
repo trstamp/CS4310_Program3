f6995c5b885395111d9d8662e256c91944003524
Tyler <35055803+trstamp@users.noreply.github.com>
2020-05-09 16:31:28 -0700

    Create MemoryManagement.java
    Creating the memory management class with methods for each policy and algorithm

---
+++ MemoryManagement.java
@@ -0,0 +1,35 @@
+public class MemoryManagement {
+
+    Process pr = new Process();
+
+    public void VSP(int policy){
+        
+    }
+
+    public void Paging(){
+
+    }
+
+    public void Segmentation(int policy){
+
+    }
+
+    public void BestFit(){
+        //Iterate through all indices and find the index value that
+        //is closest to the requested size
+
+    }
+
+    public void FirstFit(){
+        //Iterate in a for loop and in an array and as soon as you find
+        //a space that fits the requested size, choose it
+
+    }
+
+    public void WorstFit(){
+        //Iterate in a for loop in every spot in an array.
+        //The memory size with the largest value will go in the spot that
+        //has the highest value.
+
+    }
+}
