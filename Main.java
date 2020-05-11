425b3a225b166ddc122dbc51cad9fbda5fbdece9
Tyler <35055803+trstamp@users.noreply.github.com>
2020-05-11 16:28:46 -0700

    Update Main.java
    adding algorithm methods to main

--- Main.java
+++ Main.java
@@ -120,5 +120,24 @@ public class Main {
         readFile(file); //node takes this in
 
     }
+    
+    public void BestFit(){
+        //Iterate through all indices and find the index value that
+        //is closest to the requested size
+
+    }
+
+    public void FirstFit(){
+        //Iterate in a for loop and in an array and as soon as you find
+        //a space that fits the requested size, choose it
+
+    }
+
+    public void WorstFit(){
+        //Iterate in a for loop in every spot in an array.
+        //The memory size with the largest value will go in the spot that
+        //has the highest value.
+
+    }
 
 }
